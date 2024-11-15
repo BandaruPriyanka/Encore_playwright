c2a118d660b5f4dec53f004f241af3c22d94e351
Feuji-QA <kishore.b@feuji.com>
2024-11-15 13:19:31 +0530

    updated xpaths

--- playwright.config.js
+++ playwright.config.js
@@ -44,7 +44,7 @@ module.exports = defineConfig({
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
     actionTimeout: 120000,
-    headless: false,
+    headless: true,
     video: 'on',
     logLevel: 'error',
     screenshot: 'only-on-failure',
